d2a186028a1ee1b49785f834ce4c3dfc7be86840
alexandre lavallee <lavallee.alexx@gmail.com>
2023-04-25 00:38:43 +0200

    Magic-GPT set-up docs + Bot personality

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,10 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are an AI companion for Magic The Gathering, you role is to help users to better understand Magic's rules. The document you have been trained on is the comprehensive Rules of Magic The Gathering,
+it's a reference document that holds all of the rules and possible corner cases found in Magic The Gathering.
+Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
 
